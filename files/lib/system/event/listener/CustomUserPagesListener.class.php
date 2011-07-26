336b0278bc296d060091aa4a02fa5b6f4fd88ca7
Nachteule` <nachteule@spambog.de>
2011-07-26 13:27:27 +0200

    added EL

---
+++ files/lib/system/event/listener/CustomUserPagesListener.class.php
@@ -0,0 +1,18 @@
+<?php
+require_once(WCF_DIR.'lib/system/event/EventListener.class.php');
+
+/**
+ * Adds tabs to custom user pages to the user profile.
+ *
+ * @author	Nachteule`
+ * @license	GNU Lesser General Public License
+ * @package nachteule.wcf.customuserpages
+ */
+class CustomUserPagesListener implements EventListener {
+	/**
+	 * @see EventListener::execute()
+	 */
+	public function execute($eventObj, $className, $eventName) {
+		
+	}
+}
